a9c4a62dbb45eef1ff5b34fe87bedffc3a955a15
kamrulislam2 <kamrulislamABK@gmail.com>
2023-05-03 23:11:08 +0600

    chef recipe added with rating added

--- src/components/ChefCard/SingleChefCard.jsx
+++ src/components/ChefCard/SingleChefCard.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import BannerForChef from "../../layouts/BannerForChef/BannerForChef";
+import RecipeCard from "../RecipeCard/RecipeCard";
 
 const SingleChefCard = () => {
   const data = useLoaderData();
-  console.log(data);
+  const { recipes } = data;
+  //   console.log(recipes);
   return (
-    <div>
+    <div className="mb-48">
       <BannerForChef data={data}></BannerForChef>
-      <h2>This is chef single card</h2>
+      <h2 className="mt-48 font-extrabold text-5xl text-center">
+        Chef's Best Recipe
+      </h2>
+      <div className="grid grid-cols-3 mt-24 px-40 gap-8">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.recipeId} recipe={recipe}></RecipeCard>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import { FaBookmark, FaRegBookmark, FaRegStar, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
+
+const RecipeCard = ({ recipe }) => {
+  const { name, ingredients, cooking_method, rating } = recipe;
+  const [fav, setFav] = useState(true);
+
+  const handleFavorite = () => {
+    toast.success("Favorite Recipe Added!");
+    setFav(false);
+  };
+
+  return (
+    <div className="card w-96 bg-gray-900 text-primary-content">
+      <div className="card-body">
+        <h2 className="font-extrabold text-xl mb-6">{name}</h2>
+        <div>
+          <p className="text-lg mb-2">Recipe Ingredients:</p>
+          {ingredients.map((ingredient, index) => (
+            <li className="text-lg text-gray-300" key={index}>
+              {ingredient}
+            </li>
+          ))}
+        </div>
+        <p className="text-lg my-3">
+          Cooking Method:
+          <span className=" text-base text-gray-300"> {cooking_method}</span>
+        </p>
+        <hr className="border-b border-gray-600 " />
+        <div className="text-2xl flex justify-between items-center">
+          <p className="inline-flex items-center  gap-2">
+            {rating}
+            <Rating
+              placeholderRating={rating}
+              readonly
+              emptySymbol={<FaRegStar></FaRegStar>}
+              placeholderSymbol={<FaStar className="text-warning"></FaStar>}
+              fullSymbol={<FaStar></FaStar>}
+            />
+          </p>
+          <button disabled={!fav} onClick={handleFavorite}>
+            {/* <FaRegBookmark></FaRegBookmark> */}
+            {fav ? <FaRegBookmark></FaRegBookmark> : <FaBookmark></FaBookmark>}
+          </button>
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default RecipeCard;
