fba7f46aa42f2b2f513021a078c203eb2c781796
kamrulislam2 <kamrulislamABK@gmail.com>
2023-05-04 13:41:23 +0600

    Login page setup with google and github login done

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,15 +1,66 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import NavBar from "../../shared/Navbar/Navbar";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProviders";
 
 const Login = () => {
+  const { signIn, signInGoogle, signInGitHub } = useContext(AuthContext);
+  const [error, setError] = useState("");
+
+  const handleLogin = (event) => {
+    setError("");
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      });
+    form.reset();
+  };
+
+  const handleLoginGoogle = (event) => {
+    setError("");
+    event.preventDefault();
+
+    signInGoogle()
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleGitHubLogin = (event) => {
+    setError("");
+    event.preventDefault();
+
+    signInGitHub()
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="px-40 pt-12">
       <NavBar></NavBar>
       <div className="w-1/2 mx-auto text-center mb-48 ">
-        <form className="card border shadow-xl p-10">
+        <form onSubmit={handleLogin} className="card border shadow-xl p-10">
           <h2 className="font-extrabold text-4xl mb-6">Please Login</h2>
           <label className="text-start flex flex-col">
             Your Email
@@ -31,6 +82,7 @@ const Login = () => {
               placeholder="Password"
             />
           </label>
+          <p className="text-left mb-2 font-semibold text-red-600">{error}</p>
           <button
             className=" py-4 text-white text-lg font-medium rounded bg-gray-800 hover:bg-transparent hover:border hover:border-gray-800 hover:text-gray-800"
             type="submit"
@@ -39,10 +91,16 @@ const Login = () => {
           </button>
 
           <div className="mt-6 inline-flex items-center justify-center gap-6">
-            <button className="px-4 py-3 text-gray-800 text-lg font-medium rounded bg-white hover:bg-transparent border border-gray-800 hover:text-white hover:bg-gray-800 inline-flex items-center gap-2">
+            <button
+              onClick={handleLoginGoogle}
+              className="px-4 py-3 text-gray-800 text-lg font-medium rounded bg-white hover:bg-transparent border border-gray-800 hover:text-white hover:bg-gray-800 inline-flex items-center gap-2"
+            >
               <FcGoogle /> Sign In Google
             </button>
-            <button className="px-4 py-3 text-gray-800 text-lg font-medium rounded bg-white hover:bg-transparent border border-gray-800 hover:text-white hover:bg-gray-800 inline-flex items-center gap-2">
+            <button
+              onClick={handleGitHubLogin}
+              className="px-4 py-3 text-gray-800 text-lg font-medium rounded bg-white hover:bg-transparent border border-gray-800 hover:text-white hover:bg-gray-800 inline-flex items-center gap-2"
+            >
               <FaGithub /> Sign In GitHub
             </button>
           </div>
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -4,17 +4,35 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   onAuthStateChanged,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  GithubAuthProvider,
 } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const signInGitHub = () => {
+    return signInWithPopup(auth, githubProvider);
   };
 
   useEffect(() => {
@@ -26,7 +44,7 @@ const AuthProviders = ({ children }) => {
     };
   }, []);
 
-  const authInfo = { createUser, user };
+  const authInfo = { createUser, user, signIn, signInGoogle, signInGitHub };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
